3b56268f2d788ee4a6c8f171922cc46073f7b27b
L0new01f0902 <xuanphuc090207@gmail.com>
2023-12-08 07:06:23 +0700

    Firebase

---
+++ assets/js/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBXE-JEAjLGBb7_Ate1Aj-cPFlsxHIn4KE",
+  authDomain: "arena-of-valor-8c7ba.firebaseapp.com",
+  projectId: "arena-of-valor-8c7ba",
+  storageBucket: "arena-of-valor-8c7ba.appspot.com",
+  messagingSenderId: "592158854220",
+  appId: "1:592158854220:web:52b8f550e8c5b1c04338e9",
+  measurementId: "G-RVTRNFGBJR"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
